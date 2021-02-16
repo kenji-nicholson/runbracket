dde0c55e5b513b160f476bdcda2aa81224496952
Kenji Nicholson <38046992+kenji-nicholson@users.noreply.github.com>
2021-02-15 18:16:09 -0800

    Got schemas to work with database, need to implement some end points now.

--- app/api/schemas.py
+++ app/api/schemas.py
@@ -6,29 +6,22 @@ from app.models import *
 class TournamentSchema(ma.SQLAlchemySchema):
     class Meta:
         model = Tournament
-
-    tournament_id = ma.auto_field()
-    tournament_name = ma.auto_field()
-    tournament_description = ma.auto_field()
-
-
-class MatchSchema(ma.SQLAlchemySchema):
-    class Meta:
-        model = Match
-
-    match_id = ma.auto_field()
-    date = ma.auto_field()
-    participant_a_score = ma.auto_field()
-    participant_b_score = ma.auto_field()
-    participant_a = ma.auto_field()
-    participant_b = ma.auto_field()
-    match_number = ma.auto_field()
+        fields = ("tournament_id", "date", "tournament_name", "tournament_description")
 
 
 class ParticipantSchema(ma.SQLAlchemySchema):
     class Meta:
         model = Participant
+        fields = ("participant_id", "participant_name", "seed")
 
-    participant_id = ma.auto_field()
-    participant_name = ma.auto_field()
-    seed = ma.auto_field()
+
+class MatchSchema(ma.SQLAlchemySchema):
+    class Meta:
+        model = Match
+        fields = ("match_id", "date", "participant_a_score", "participant_b_score", "participant_a", "participant_b",
+                  "match_number")
+
+    participant_a = ma.Nested(ParticipantSchema)
+    participant_b = ma.Nested(ParticipantSchema)
+
+
