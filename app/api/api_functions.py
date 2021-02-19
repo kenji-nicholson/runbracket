72c6d67d8431f07db22e171d70b8490e0c18e65d
Kenji Nicholson <38046992+kenji-nicholson@users.noreply.github.com>
2021-02-18 22:28:45 -0800

    Added some routing code, as well as pagination support for tournaments

--- app/api/api_functions.py
+++ app/api/api_functions.py
@@ -1,6 +1,8 @@
 """
 Utility functions for api
 """
+from flask import url_for
+
 from app.api import bp
 
 
@@ -19,3 +21,27 @@ def register_api(view, endpoint, url, pk='id', pk_type='int'):
     bp.add_url_rule(url, view_func=view_func, methods=['POST', ])
     bp.add_url_rule('%s<%s:%s>' % (url, pk_type, pk), view_func=view_func,
                     methods=['GET', 'PUT', 'DELETE'])
+
+
+class PaginatedAPIMixin(object):
+    @staticmethod
+    def get_paginated_collection(query, schema, page, per_page, endpoint, **kwargs):
+        resources = query.paginate(page, per_page, False)
+        data = {
+            'items': schema.dump(resources),
+            '_meta': {
+                'page': page,
+                'per_page': per_page,
+                'total_pages': resources.pages,
+                'total_items': resources.total
+            },
+            '_links': {
+                'self': url_for(endpoint, page=page, per_page=per_page,
+                                **kwargs),
+                'next': url_for(endpoint, page=page + 1, per_page=per_page,
+                                **kwargs) if resources.has_next else None,
+                'prev': url_for(endpoint, page=page - 1, per_page=per_page,
+                                **kwargs) if resources.has_prev else None
+            }
+        }
+        return data
--- app/api/tournament_routes.py
+++ app/api/tournament_routes.py
@@ -1,22 +1,36 @@
 """
 Endpoints for tournament related API calls.
 """
+from flask import request, jsonify
 from flask.views import MethodView
 from app.models import Tournament, Match, Participant
-from app.api.api_functions import register_api
+from app.api.api_functions import register_api, PaginatedAPIMixin
+from app.api.schemas import TournamentSchema, MatchSchema, ParticipantSchema
+from app.api.errors import bad_request
 
 
-class TournamentAPI(MethodView):
+class TournamentAPI(MethodView, PaginatedAPIMixin):
     """
     Endpoints for managing/viewing overall tournament information.
     """
+    tournament_schema = TournamentSchema()
+    tournaments_schema = TournamentSchema(many=True)
+
     def get(self, tournament_id):
         if tournament_id is None:
-            pass
+            page = request.args.get('page', 1, type=int)
+            per_page = min(request.args.get('per_page', 10, type=int), 100)
+            data = self.get_paginated_collection(Tournament.query, self.tournaments_schema, page, per_page,
+                                                 'tournament_api')
+            return jsonify(data)
         else:
-            pass
+            tournament = Tournament.query.get(tournament_id)
+            if not tournament:
+                return bad_request('Tournament does not exist!')
+            return self.tournament_schema.dump(tournament)
 
     def post(self):
+        # TODO : Call method for creating tournament bracket
         pass
 
     def delete(self, tournament_id):
@@ -30,14 +44,23 @@ class TournamentMatchesAPI(MethodView):
     """
     Endpoints for managing/viewing matches belonging to a specified tournament.
     """
+
+    match_schema = MatchSchema()
+
     def get(self, tournament_id, match_id):
         if match_id is None:
+            # retrieve all matches for tournament
+            # TODO: figure out how to best return all matches in a tournament
             pass
         else:
-            pass
-
-    def post(self, tournament_id):
-        pass
+            # retrieve single match
+            tournament = Tournament.query.get(tournament_id)
+            if not tournament:
+                return bad_request('Tournament does not exist!')
+            match = Match.query.get(match_id)
+            if not match:
+                return bad_request('Match does not exist!')
+            return self.match_schema.dump(match)
 
     def delete(self, tournament_id, match_id):
         pass
@@ -50,14 +73,26 @@ class TournamentParticipantsAPI(MethodView):
     """
     Endpoints for managing/viewing participants within a specified tournament.
     """
+
+    participants_schema = ParticipantSchema(many=True)
+    participant_schema = ParticipantSchema()
+
     def get(self, tournament_id, participant_id):
         if participant_id is None:
-            pass
+            # retrieve all participants for tournament
+            tournament = Tournament.query.get(tournament_id)
+            if not tournament:
+                return bad_request('Tournament does not exist!')
+            return self.participants_schema.dump(tournament.participants)
         else:
-            pass
-
-    def post(self, tournament_id):
-        pass
+            # retrieve single participant
+            tournament = Tournament.query.get(tournament_id)
+            if not tournament:
+                return bad_request('Tournament does not exist!')
+            participant = Participant.query.get(participant_id)
+            if not participant:
+                return bad_request('Participant does not exist!')
+            return self.participant_schema.dump(participant)
 
     def delete(self, tournament_id, participant_id):
         pass
