15759275e49c9b292cc23a16c59a82bcca969e05
Kenji Nicholson <38046992+kenji-nicholson@users.noreply.github.com>
2021-08-18 23:02:01 -0700

    Made navbar fade in and out on scroll

--- client/src/components/NavigationBar.tsx
+++ client/src/components/NavigationBar.tsx
@@ -1,15 +1,52 @@
-import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import {
+  AppBar,
+  Theme,
+  Toolbar,
+  Typography,
+  useScrollTrigger,
+} from "@material-ui/core";
 import { makeStyles, createStyles } from "@material-ui/core/styles";
+import { useState, useEffect, useRef } from "react";
 import React from "react";
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    appBar: {
+      transition: theme.transitions.create(["background-color", "box-shadow"], {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.standard,
+      }),
+    },
+    appBarScrolled: {
+      boxShadow: "1",
+    },
+    appBarNotScrolled: {
+      boxShadow: "none",
+      backgroundColor: "transparent",
+    },
+  })
+);
+
 const NavigationBar = () => {
+  const classes = useStyles();
+
+  const scrollTrigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 80,
+  });
 
   return (
-    <AppBar position="static">
+    <AppBar
+      position="fixed"
+      color="default"
+      className={`${classes.appBar} ${
+        scrollTrigger === false
+          ? classes.appBarNotScrolled
+          : classes.appBarScrolled
+      }`}
+    >
       <Toolbar>
-        <Typography>
-          PP3 
-        </Typography>
+        <Typography>PP3</Typography>
       </Toolbar>
     </AppBar>
   );
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import { ThemeProvider } from "@material-ui/styles";
 import App from "./App";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
 
 ReactDOM.render(
   <Provider store={store}>
-      <Router>
-        <React.StrictMode>
-          <App />
-        </React.StrictMode>
-      </Router>
-    </Provider>,
+    <Router>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
